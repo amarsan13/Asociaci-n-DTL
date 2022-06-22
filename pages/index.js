aee925aece9dca83976c9008dd15f03878c4be6a
amarsan13 <68304994+amarsan13@users.noreply.github.com>
2022-06-22 13:42:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,10 @@ export default function Home() {
         <Header title="Bienvenid@s a la página temporal de Asociación DTL" />
         <p className="description">
           Si estás interesad@ en asociarte, pero antes te gustaría informarte más, os dejamos algunos enlaces de interés.
-        <br>
           <a href="https://mega.nz/file/cWN0XTrD#tguPI2SfdA09VsyoVITNMjVp1xHJQn6nnP-We7ipVXQ"><b>Ver Estatutos</b></a>
-        <br>
           <a href="https://t.me/+7XRSUgEwbkFjZDY0"><b>Unirme al grupo de Telegram</b></a>
-        <br>
           <a href="https://p.berrly.com/ASODTL/registration/2594?lang=es"><b>Ir al formulario de inscripción</b></a>
-        <br>  
           <a href="https://vimeo.com/717051965"><b>Ver vídeo explicativo de la zona privada de socios</b></a>
-        </p>
       </main>
 
       <Footer />
